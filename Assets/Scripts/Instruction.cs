3fd819c54447e9e8f1cba0d3eb44b3f689577b62
abdulbaasitt <95265491+abdulbaasitt@users.noreply.github.com>
2022-03-14 17:45:22 +0000

    Update Instruction.cs

--- Assets/Scripts/Instruction.cs
+++ Assets/Scripts/Instruction.cs
@@ -1,54 +1,54 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class Instruction : MonoBehaviour, INetworkComponent, INetworkObject
-{
-    // Copied from Cylinder Score (Ahmed)
-    NetworkId INetworkObject.Id => new NetworkId(3190);
+// public class Instruction : MonoBehaviour, INetworkComponent, INetworkObject
+// {
+//     // Copied from Cylinder Score (Ahmed)
+//     NetworkId INetworkObject.Id => new NetworkId(3190);
 
-    private NetworkContext context;
+//     private NetworkContext context;
 
-    public int Score = 0;
+//     public int Score = 0;
 
-    public TextHandler textHandler;
+//     public TextHandler textHandler;
 
-    void INetworkComponent.ProcessMessage(ReferenceCountedSceneGraphMessage message)
-    {
-        var msg = message.FromJson<Message>();
-        Score = msg.score;
-    }
+//     void INetworkComponent.ProcessMessage(ReferenceCountedSceneGraphMessage message)
+//     {
+//         var msg = message.FromJson<Message>();
+//         Score = msg.score;
+//     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        //from ahmed context thingy
-        context = NetworkScene.Register(this);        
-    }
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         //from ahmed context thingy
+//         context = NetworkScene.Register(this);        
+//     }
 
-    struct Message
-    {
-        public int score;
+//     struct Message
+//     {
+//         public int score;
 
-    }
+//     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        //from ahmed  conversion of 
-        textHandler.tm.text = Score.ToString();
-    }
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         //from ahmed  conversion of 
+//         textHandler.tm.text = Score.ToString();
+//     }
     
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.name == "Cylinder")
-        {
-            Score++;
-            Debug.Log("collide");
-            Message message;
-            message.score = Score;
-            context.SendJson(message);
-        }
-    }
+//     private void OnTriggerEnter(Collider other)
+//     {
+//         if(other.name == "Cylinder")
+//         {
+//             Score++;
+//             Debug.Log("collide");
+//             Message message;
+//             message.score = Score;
+//             context.SendJson(message);
+//         }
+//     }
 
-}
+// }
