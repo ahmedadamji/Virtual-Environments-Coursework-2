db0d1847087e8734ddfe63f0c1467a6502fdf41c
Eduard Lavrishchev <e.lavrishchev@gmail.com>
2022-03-21 23:22:50 +0000

    Picking up big objects test 1

---
+++ Assets/Scripts/MoveAndSyncBig.cs
@@ -0,0 +1,70 @@
+﻿using Ubiq.Messaging;
+using Ubiq.XR;
+using UnityEngine;
+using PlayerNumber = System.Int32;
+
+public class MoveAndSyncBig : MonoBehaviour, IGraspable, INetworkComponent, INetworkObject
+{
+    public Hand grasped;
+    public string id;
+    private AccessManager accessManager;
+
+    private bool isOwned;
+    private NetworkContext context;
+    private float distance;
+
+    private HandController[] handControllers;
+
+    public BigHandler bigHandler;
+
+    private void Awake()
+    {
+        accessManager = GetComponent<AccessManager>();
+        handControllers = FindObjectsOfType<HandController>();
+    }
+
+    private void Start()
+    {
+        context = NetworkScene.Register(this);
+    }
+
+    private void Update()
+    {
+        if (grasped)
+        {
+            bigHandler.MoverReady(this);
+        }
+        else
+        {
+            bigHandler.MoverReleased(this);
+        }
+    }
+
+    void IGraspable.Grasp(Hand controller)
+    {
+        grasped = controller;
+    }
+
+    void IGraspable.Release(Hand controller)
+    {
+        grasped = null;
+    }
+
+    void INetworkComponent.ProcessMessage(ReferenceCountedSceneGraphMessage message)
+    {
+        var msg = message.FromJson<Message>();
+        grasped = msg.Grasped;
+    }
+
+    NetworkId INetworkObject.Id => new NetworkId(id);
+
+    public struct Message
+    {
+        public Hand Grasped;
+
+        public Message(Hand grasped)
+        {
+            Grasped = grasped;
+        }
+    }
+}
