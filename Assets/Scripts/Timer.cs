a4ab6d920aab8c12638cfc8a71bcd3cf19bd1d54
kennedy12335 <68738672+kennedy12335@users.noreply.github.com>
2022-03-21 00:25:20 +0000

    Timer added
    I added the timer. The format of the timer may need to change tho

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Ubiq.Messaging;
+using Ubiq.XR;
+using UnityEngine;
+
+public class Timer : MonoBehaviour, INetworkComponent, INetworkObject
+{
+    NetworkId INetworkObject.Id => new NetworkId(4000);
+
+    private NetworkContext context;
+
+    public float timeValue = 0;
+
+    public TextHandler textHandler;
+
+    void INetworkComponent.ProcessMessage(ReferenceCountedSceneGraphMessage message)
+    {
+        var msg = message.FromJson<Message>();
+        timeValue = msg.time;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        context = NetworkScene.Register(this);
+    }
+
+    struct Message
+    {
+        public float time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Score += 1;
+        timeValue += Time.deltaTime;
+        DisplayText(timeValue);
+    }
+    void DisplayText(float timeToDisplay)
+    {
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        string minute_str = minutes.ToString();
+        string seconds_str = seconds.ToString();
+        GetComponent<TextMesh>().text = minute_str + ":" + seconds_str;
+
+        Message message;
+        message.time = timeValue;
+        context.SendJson(message);
+    }
+    
+}
