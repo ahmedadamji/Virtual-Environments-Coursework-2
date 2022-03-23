a3ee486fca0fb34c0009e450fed161263df6b362
Eduard Lavrishchev <e.lavrishchev@gmail.com>
2022-03-23 13:33:09 +0000

    push

--- Assets/Motherboard.cs
+++ Assets/Motherboard.cs
@@ -1,19 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Ubiq.Messaging;
 using UnityEngine;
 
-public class Motherboard : MonoBehaviour
+public class Motherboard : MonoBehaviour, INetworkComponent, INetworkObject
 {
     public int partsReplacedCount;
 
     public int totalPartsNumber = 10;
     
+    NetworkId INetworkObject.Id => new NetworkId(3457483);
+
+    private NetworkContext context;
+    
+    void Start()
+    {
+        context = NetworkScene.Register(this);
+    }
     public void PartReplaced()
     {
         partsReplacedCount++;
         if (partsReplacedCount == totalPartsNumber)
         {
             MoveMotherboard();
+            context.SendJson(new Message(partsReplacedCount));
+        }
+    }
+
+    struct Message
+    {
+        public int PartsReplacedCount;
+
+        public Message(int totalParts)
+        {
+            PartsReplacedCount = totalParts;
         }
     }
 
@@ -22,9 +42,16 @@ public class Motherboard : MonoBehaviour
         GetComponent<MoveAndSync>().enabled = true;
         GetComponent<AccessManager>().locked = false;
         GetComponent<AccessManager>().available = true;
+        GetComponent<AccessManager>().shareable = true;
         //GetComponent<Rigidbody>().isKinematic = true;
         //GetComponent<Rigidbody>().useGravity = false;
         GetComponent<BoxCollider>().enabled = true;
         GetComponent<Snapper>().enabled = true;
     }
+
+    public void ProcessMessage(ReferenceCountedSceneGraphMessage message)
+    {
+        var msg = message.FromJson<Message>();
+        partsReplacedCount = msg.PartsReplacedCount;
+    } 
 }
